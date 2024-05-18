5736e33ec63499493a4b7e490fab5e8ce5336c14
Daniel Lagos <9258221+danlagos@users.noreply.github.com>
2024-05-18 00:08:55 +0000

    working dynamic param.

--- fastapi_my_work/project_1/books.py
+++ fastapi_my_work/project_1/books.py
@@ -17,8 +17,17 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
- 
 
-@app.get("/books/{dynamic_param}")
-async def read_all_books(dynamic_param: str):
-    return {'dynamic_param': dynamic_param}
+
+
+@app.get("/books/{book_title}")
+async def read_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    return [book for book in BOOKS if book.get('category').casefold() == 
+            category.casefold()]
